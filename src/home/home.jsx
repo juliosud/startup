1c739f30798b2e6924293a3249f1b7cd38aef2f0
julisud <matheusbyui@gmail.com>
2025-02-13 22:50:30 -0700

    REACT - 6 draft

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -2,8 +2,37 @@ import React from 'react';
 
 export function Home() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Home displayed here</div>
-    </main>
+    <main>
+            <div className="box-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Calories</th>
+                            <th>Protein</th>
+                            <th>Carbs</th>
+                            <th>Fat</th>
+                        </tr>
+                    </thead>
+                    
+                    <tbody>
+                        <tr>
+                            <td>2200</td>
+                            <td>136</td>
+                            <td>289</td>
+                            <td>63</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div className="box-container">
+            <form method="post">
+                <div>
+                    <textarea name="Hungry?" placeholder="What would you like to eat?"></textarea>
+                    <button type="submit">Go</button>
+                </div>
+            </form>
+        </div>
+        </main>
   );
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
-    </main>
+    <main>
+            <div className="box-container">
+            <img src="https://media.istockphoto.com/id/1395140920/photo/peach-blueberry-and-arugula-fresh-fruit-salad-with-cheese-and-almond-nuts-top-view.jpg?s=612x612&w=0&k=20&c=IrRb7_0bFeO5uD0tfBoJxTElJHQ372li_ODfPEd7Vdk=" width="450" height="250"/> 
+            <h4>login</h4>
+            <form method="post">
+                <div>
+                    <input type="text" placeholder="cougars@byu.edu"/>
+                </div>
+                <div>
+                    <input type="password" placeholder="***"/>
+                </div>
+                <button type="submit">Login</button>
+                <button type="submit">Create</button>
+            </form>
+        </div>
+        </main>
   );
 }
